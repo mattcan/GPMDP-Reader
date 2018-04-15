a6018d362721211691dcab2bf2c2f5db4ce79f2c
Matthew Cantelon <gh@matthewcantelon.ca>
2018-04-14 23:53:35 -0700

    feat: first whack at hanlding multiple apis automatically

--- Program.cs
+++ Program.cs
@@ -1,12 +1,20 @@
-﻿using System;
+﻿using gpmdp_rdr.Providers;
+using System;
+using System.Collections.Generic;
 using System.IO;
-using gpmdp_rdr.Providers;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace gpmdp_rdr
 {
+    struct ProviderEntry {
+        public IProvider Provider;
+        public int Weight;
+    };
+
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             // TODO this might be wrong when its compiled to an executable
             if(args.Length != 2) {
@@ -15,8 +23,33 @@ namespace gpmdp_rdr
                 return;
             }
 
-            JsonApi w = new JsonApi(args[0]);
-            w.Start(args[1]);
+            List<ProviderEntry> providers = new List<ProviderEntry>() {
+                new ProviderEntry() {
+                    Provider = new JsonApi(args[0]),
+                    Weight = 10
+                },
+                new ProviderEntry() {
+                    Provider = await WebsocketApi.CreateWebsocketApi(),
+                    Weight = 100
+                }
+            };
+
+            IProvider provider = null;
+
+           try { 
+                provider = providers
+                    .Where(entry => entry.Provider.IsUseable())
+                    .OrderBy(entry => entry.Weight)
+                    .Single()
+                    .Provider;
+           } catch (Exception e) {
+               Console.WriteLine("No working API, please enable one in Google Play Music Desktop Player");
+               Program.ExitWith(ExitCode.NO_WORKING_PROVIDER);
+           }
+
+           provider.Start(args[1]);
+
+           Console.ReadKey();
         }
 
         public static void ExitWith(ExitCode Code) {
